2e3cde75e91b9ad692f41749480213601e000748
JC <jasonqc9103@gmail.com>
2023-10-19 11:49:07 -0400

    establish front end and Back end

---
+++ src/main/java/com/jc/e_commerce/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.jc.e_commerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
